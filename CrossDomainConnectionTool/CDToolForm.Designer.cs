1679a7a3e3f2d022bef47d2794f122d73c2ee30c
doymturner <dturner@centralkytechnology.com>
2014-04-16 13:12:57 -0400

    * Added a combo box for selecting SSMS version
    *Updated back-end to open the correct version of SSMS

--- CrossDomainConnectionTool/CDToolForm.Designer.cs
+++ CrossDomainConnectionTool/CDToolForm.Designer.cs
@@ -32,6 +32,8 @@
             this.domainTxt = new System.Windows.Forms.TextBox();
             this.userTxt = new System.Windows.Forms.TextBox();
             this.connectBtn = new System.Windows.Forms.Button();
+            this.versionComboBox = new System.Windows.Forms.ComboBox();
+            this.ssmsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // domainTxt
@@ -52,20 +54,43 @@
             // 
             // connectBtn
             // 
-            this.connectBtn.Location = new System.Drawing.Point(150, 64);
+            this.connectBtn.Location = new System.Drawing.Point(150, 92);
             this.connectBtn.Name = "connectBtn";
             this.connectBtn.Size = new System.Drawing.Size(75, 23);
-            this.connectBtn.TabIndex = 2;
+            this.connectBtn.TabIndex = 3;
             this.connectBtn.Text = "Connect";
             this.connectBtn.UseVisualStyleBackColor = true;
             this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // versionComboBox
+            // 
+            this.versionComboBox.FormattingEnabled = true;
+            this.versionComboBox.Items.AddRange(new object[] {
+            "2008",
+            "2012"});
+            this.versionComboBox.Location = new System.Drawing.Point(104, 65);
+            this.versionComboBox.Name = "versionComboBox";
+            this.versionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.versionComboBox.TabIndex = 2;
+            // 
+            // ssmsLabel
+            // 
+            this.ssmsLabel.AutoSize = true;
+            this.ssmsLabel.Enabled = false;
+            this.ssmsLabel.Location = new System.Drawing.Point(30, 65);
+            this.ssmsLabel.Name = "ssmsLabel";
+            this.ssmsLabel.Size = new System.Drawing.Size(78, 13);
+            this.ssmsLabel.TabIndex = 4;
+            this.ssmsLabel.Text = "SSMS Version:";
             // 
             // CDToolForm
             // 
             this.AcceptButton = this.connectBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 99);
+            this.ClientSize = new System.Drawing.Size(231, 124);
+            this.Controls.Add(this.versionComboBox);
+            this.Controls.Add(this.ssmsLabel);
             this.Controls.Add(this.connectBtn);
             this.Controls.Add(this.userTxt);
             this.Controls.Add(this.domainTxt);
@@ -82,6 +107,8 @@
         private System.Windows.Forms.TextBox domainTxt;
         private System.Windows.Forms.TextBox userTxt;
         private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.ComboBox versionComboBox;
+        private System.Windows.Forms.Label ssmsLabel;
     }
 }
 
--- CrossDomainConnectionTool/CDToolForm.cs
+++ CrossDomainConnectionTool/CDToolForm.cs
@@ -26,11 +26,20 @@ namespace CrossDomainConnectionTool
         {
             string dom = domainTxt.Text;
             string user = userTxt.Text;
-            //Make sure we are creating the correct file path based on the system bit size assuming default installtion of SQL Server
-            string path = Directory.Exists(@"C:\Program Files (x86)") ? @"C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\VSShell\Common7\IDE\Ssms.exe" : @"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\VSShell\Common7\IDE\Ssms.exe";
-            PSConnection ps = new PSConnection();
+            string path; 
+            //Check which version of SSMS the user want
+            if (versionComboBox.Text.Equals("2012"))
+            {
+                path = Directory.Exists(@"C:/Program Files (x86)") ? @"C:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Ssms.exe" : @"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\Ssms.exe";
+            }else{
+                    //if the user is after VS2008, make sure we are creating the correct file path based on the system bit size assuming default installtion of SQL Server
+                    path = Directory.Exists(@"C:\Program Files (x86)") ? @"C:\Program Files (x86)\Microsoft SQL Server\100\Tools\Binn\VSShell\Common7\IDE\Ssms.exe" : @"C:\Program Files\Microsoft SQL Server\100\Tools\Binn\VSShell\Common7\IDE\Ssms.exe";
+                }
+                PSConnection ps = new PSConnection();
             ps.buildConnection(dom, user, path);
         }
+
+       
 
     }
 }
